209cacdafee00c0e2d17c480484b7a5fdb592638
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2013-05-07 22:32:25 -0300

    Problem 0: Query Twitter with Python

--- assignment1/print.py
+++ assignment1/print.py
@@ -22,7 +22,7 @@ while page in range(0, 10):
         results = dict['results']
         for res in results:
                 text = res['text'].encode('utf-8')
-                text = re.sub(r"\s+", ' ', text)
+                text = re.sub(r'\s+', ' ', text)
                 #print "[%s]" % text
                 print text
 
