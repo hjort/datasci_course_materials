1ed940ef72f1cc6181be5e1cbe1ab56051213446
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2013-05-07 23:34:36 -0300

    Problem 5: Which State is happiest?

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -20,14 +20,37 @@ def normalize(text):
 	# awk '{print$1}' AFINN-111.txt | sed 's/[a-z]\+//g' | sort -u
 	return re.sub('[^a-z0-9 -]', '', text.lower())
 
+# retrieved from http://www.census.gov/geo/www/ansi/statetables.html
+# awk 'BEGIN{FS="\t"}{print "\""$1"\""": \""$3"\""", "}' us-states.txt
+us_codes = {
+	"Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR", 
+	"California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", 
+	"District of Columbia": "DC", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI",
+	"Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", 
+	"Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD", 
+	"Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", 
+	"Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+	"New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", 
+	"North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK", 
+	"Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC", 
+	"South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT",
+	"Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", 
+	"Wisconsin": "WI", "Wyoming": "WY"
+}
+
+def get_state_code(name):
+	if name in us_codes:
+		return us_codes[name]
+	else:
+		return None
+
 def read_tweets(fp):
 	count = 0
 	for line in fp:
-		#print line
 		tweet = json.loads(line)
-		#print tweet
 		if 'created_at' in tweet:
 			if 'lang' in tweet and tweet['lang'] == 'en':
+
 				text = tweet['text'].encode('utf-8')
 				norm = normalize(text)
 
@@ -52,8 +75,17 @@ def read_tweets(fp):
 
 				if cc == 'US':
 
-					# retrieve the state code
+					# retrieve the state code (e.g., "Los Angeles, CA")
 					sc = pfn[pfn.rfind(", ") + 2 : ]
+
+					# sometimes it has another format (e.g., "Florida, US")
+					if sc == 'US':
+						sc = get_state_code(pfn[ : pfn.rfind(", ")])
+					#print "%s => %s" % (pfn, sc)
+					if sc is None:
+						continue
+
+					# append or sum the state to the dict
 					if sc not in states:
 						states[sc] = 0
 					states[sc] += score
@@ -69,12 +101,22 @@ def print_state_scores():
 	for state, score in states.iteritems():
 		print "%s %d" % (state, score)
 
+def happiest_state():
+	happiest = None
+	greatest = 0
+	for state, score in states.iteritems():
+		if score > greatest or happiest is None:
+			happiest = state
+			greatest = score
+	return happiest 
+
 def main():
 	sent_file = open(sys.argv[1])
 	tweet_file = open(sys.argv[2])
 	build_dict(sent_file)
 	read_tweets(tweet_file)
-	print_state_scores()
+	#print_state_scores()
+	print happiest_state()
 
 if __name__ == '__main__':
 	main()
