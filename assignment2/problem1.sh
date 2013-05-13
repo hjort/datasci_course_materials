dbdecb5f1421ed0f1d20644519d0a83e4129bebb
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2013-05-13 15:48:27 -0300

    (e) big documents and (f) two words

--- assignment2/problem1.sh
+++ assignment2/problem1.sh
@@ -38,8 +38,19 @@ echo "d) $sql"
 runsql "$sql" > count.data
 countsql "$sql" > count.txt
 
+# (e) big documents Write a SQL statement to find all documents that have more than 300 total terms, including duplicate terms.
+sql="SELECT a.docid FROM frequency a JOIN frequency b ON (a.docid = b.docid) WHERE a.term = 'transactions' AND b.term = 'world'"
+echo "e) $sql"
+runsql "$sql" > big_documents.data
+countsql "$sql" > big_documents.txt
+
+# (f) two words: Write a SQL statement to count the number of unique documents that contain both the word 'transactions' and the word 'world'. 
+sql="SELECT a.docid FROM frequency a JOIN frequency b ON (a.docid = b.docid) WHERE a.term = 'transactions' AND b.term = 'world'"
+echo "f) $sql"
+runsql "$sql" > two_words.data
+countsql "$sql" > two_words.txt
 
 echo
 echo "Showing results..."
-head *.{txt,data}
+head *.txt
 
