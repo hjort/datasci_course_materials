857c7c4e6e3d22601672cf2f84f6ed0a949d1b97
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2013-05-13 12:42:07 -0300

    (b) select project

--- assignment2/problem1.sh
+++ assignment2/problem1.sh
@@ -4,9 +4,27 @@
 
 echo "Running queries..."
 
+function runsql {
+	sqlite3 reuters.db "$@"
+}
+
+function countsql {
+	sqlite3 reuters.db "SELECT count(1) FROM ($@)"
+}
+
 # (a) select: Write a query that is equivalent to the following relational algebra expression.
-sqlite3 reuters.db "select count(1) from frequency where docid = '10398_txt_earn'" > select.txt
+sql="SELECT * FROM frequency WHERE docid = '10398_txt_earn'"
+echo "a) $sql"
+runsql "$sql" > select.data
+countsql "$sql" > select.txt
 
 # (b) select project: Write a SQL statement that is equivalent to the following relational algebra expression.
+sql="SELECT term FROM frequency WHERE docid = '10398_txt_earn' AND count = 1"
+echo "b) $sql"
+runsql "$sql" > select_project.data
+countsql "$sql" > select_project.txt
 
+echo
+echo "Showing results..."
+head *.{txt,data}
 
